a2e4a00336dae82e5302d34f0241a747a96d9f4a
Shannon Grantski <grantski@pm.me>
2021-01-08 09:54:07 -0800

    build Engineer model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,1 +1,22 @@
-using System.Collections.Generic; // if listsnamespace Factory.Models{	public class Engineer	{		// properties, constructors, methods, etc. go here	}}
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+	public class Engineer
+	{
+		public Engineer()
+		{}
+		public int EngineerId {get;set;}
+		public string EngineerName {get;set;}
+		public bool EngineerWorkStatus {get;set;}
+
+		[DisplayName ("")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+		public DateTime EngineerHireDate {get;set;}
+		public DateTime EngineerNextCertificationDate {get;set;}
+	}
+}
