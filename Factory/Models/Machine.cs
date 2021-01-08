c990b4009c1239a0d0478713256f6b43be72ea5b
Shannon Grantski <grantski@pm.me>
2021-01-08 09:58:55 -0800

    build Machine model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,1 +1,23 @@
-using System.Collections.Generic; // if listsnamespace Factory.Models{	public class Machine	{		// properties, constructors, methods, etc. go here	}}
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+	public class Machine
+	{
+		public Machine()
+		{}
+		public int MachineId {get;set;}
+		public string MachineName {get;set;}
+		public bool MachineOnlineStatus {get;set;}
+
+		[DisplayName ("")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:yyyy-MM-dd}")]
+		public DateTime MachineInstallDate {get;set;}
+		public DateTime MachineNextInspectionDate {get;set;}
+
+	}
+}
