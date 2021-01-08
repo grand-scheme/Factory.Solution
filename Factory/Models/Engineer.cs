88511ed4c65af8cc3822b2ffbf41ba6a2fe315a8
Shannon Grantski <grantski@pm.me>
2021-01-08 13:58:49 -0800

    tie to EngineerMachine model

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -8,15 +8,19 @@ namespace Factory.Models
 	public class Engineer
 	{
 		public Engineer()
-		{}
+		{
+			this.Machines = new HashSet<EngineerMachine>();
+		}
 		public int EngineerId {get;set;}
 		public string EngineerName {get;set;}
 		public bool EngineerWorkStatus {get;set;}
 
-		[DisplayName ("")]
+		[DisplayName ("name")]
 		[DataType(DataType.Date)]
 		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
 		public DateTime EngineerHireDate {get;set;}
 		public DateTime EngineerNextCertificationDate {get;set;}
+
+		public virtual ICollection<EngineerMachine> Machines {get;set;}
 	}
 }
