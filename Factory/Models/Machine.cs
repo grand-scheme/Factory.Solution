f7e9cb94070445ed752be97b57144c076e40f71b
Shannon Grantski <grantski@pm.me>
2021-01-08 15:18:28 -0800

    add icollection to machine model

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,16 +8,19 @@ namespace Factory.Models
 	public class Machine
 	{
 		public Machine()
-		{}
+		{
+			this.Engineers = new HashSet<EngineerMachine>();
+		}
 		public int MachineId {get;set;}
 		public string MachineName {get;set;}
 		public bool MachineOnlineStatus {get;set;}
 
-		[DisplayName ("")]
+		// [DisplayName ("")]
 		[DataType(DataType.Date)]
 		[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:yyyy-MM-dd}")]
 		public DateTime MachineInstallDate {get;set;}
 		public DateTime MachineNextInspectionDate {get;set;}
 
+		public virtual ICollection<EngineerMachine> Engineers {get;set;}
 	}
 }
