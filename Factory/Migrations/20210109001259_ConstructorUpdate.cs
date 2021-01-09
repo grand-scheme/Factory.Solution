8e8b38f10c3cb24d2cb4e3eca0cd2d3f0f7e62dc
Shannon Grantski <grantski@pm.me>
2021-01-08 16:39:59 -0800

    add migration for updated datatype

---
+++ Factory/Migrations/20210109001259_ConstructorUpdate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class ConstructorUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MachineOnlineStatus",
+                table: "Machines",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EngineerWorkStatus",
+                table: "Engineers",
+                nullable: true,
+                oldClrType: typeof(bool));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "MachineOnlineStatus",
+                table: "Machines",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EngineerWorkStatus",
+                table: "Engineers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
