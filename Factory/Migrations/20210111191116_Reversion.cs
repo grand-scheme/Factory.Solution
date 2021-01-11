d4492094ffaa989bdfb8e55c4e419a8c795b5a60
Shannon Grantski <grantski@pm.me>
2021-01-11 11:16:25 -0800

    fix typo

---
+++ Factory/Migrations/20210111191116_Reversion.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Reversion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineOnlineStatus",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "EngineerWorkStatus",
+                table: "Engineers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MachineOnlineStatus",
+                table: "Machines",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EngineerWorkStatus",
+                table: "Engineers",
+                nullable: true);
+        }
+    }
+}
