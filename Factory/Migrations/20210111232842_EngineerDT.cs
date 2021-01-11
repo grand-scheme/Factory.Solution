f88606682b9db9a7faf74645c1d2a1d71159a529
Shannon Grantski <grantski@pm.me>
2021-01-11 15:31:48 -0800

    add migration for Engineer DateTime fields

---
+++ Factory/Migrations/20210111232842_EngineerDT.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class EngineerDT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EngineerHireDate",
+                table: "Engineers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EngineerNextCertificationDate",
+                table: "Engineers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EngineerHireDate",
+                table: "Engineers");
+
+            migrationBuilder.DropColumn(
+                name: "EngineerNextCertificationDate",
+                table: "Engineers");
+        }
+    }
+}
