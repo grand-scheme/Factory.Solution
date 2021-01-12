4036d4cbe04eca63372728a844a92d001e8bf17a
Shannon Grantski <grantski@pm.me>
2021-01-11 18:47:07 -0800

    add ViewBag

--- Factory/Controllers/SharedController.cs
+++ Factory/Controllers/SharedController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
 	public class SharedController : Controller
 	{
+    private readonly FactoryContext _db;
+		public SharedController(FactoryContext db)
+		{
+			_db = db;
+		}
 
 		[HttpGet("/")]
 		public ActionResult Index()
 		{
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
 			return View();
 		}
-
 	}
 }
