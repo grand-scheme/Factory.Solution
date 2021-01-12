76ed7aff456de609f77e072bc52944634a685f7c
Shannon Grantski <grantski@pm.me>
2021-01-11 19:38:02 -0800

    add migration for class Descriptions

---
+++ Factory/Migrations/20210112033726_Descriptions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Descriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MachineDescription",
+                table: "Machines",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EngineerDescription",
+                table: "Engineers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineDescription",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "EngineerDescription",
+                table: "Engineers");
+        }
+    }
+}
