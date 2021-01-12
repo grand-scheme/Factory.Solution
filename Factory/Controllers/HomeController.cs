b7b3b818bbf3e751ff458bb154994606d1fd120a
Shannon Grantski <grantski@pm.me>
2021-01-11 19:16:11 -0800

    successfully retrieve machine list on index

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,1 +1,44 @@
-using Microsoft.AspNetCore.Mvc;namespace Factory.Controllers{	public class HomeController : Controller	{		[HttpGet("/")]		public ActionResult Index()		{			return View();		}	}}
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Factory.Models;
+using System.Linq;
+
+// namespace Factory.Controllers
+// {
+// 	public class HomeController : Controller
+// 	{
+
+// 		[HttpGet("/")]
+// 		public ActionResult Index()
+// 		{
+// 			return View();
+// 		}
+
+// 	}
+// }
+
+
+namespace Factory.Controllers
+{
+	public class HomeController : Controller
+	{
+    private readonly FactoryContext _db;
+		public HomeController(FactoryContext db)
+		{
+			_db = db;
+		}
+
+		[HttpGet("/")]
+		public ActionResult Index()
+		{
+      List<Machine> machineList = _db.Machines.ToList();
+      ViewBag.Machines = machineList;
+      // ViewBag.Machines = new SelectList(_db.Machines, "MachineId", "Name");
+			return View();
+		}
+	}
+}
--- Factory/Controllers/SharedController.cs
+++ Factory/Controllers/SharedController.cs
@@ -1,22 +1,29 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Factory.Models;
+// using System.Data;
+// using System.Configuration;
+// using System.Data.SqlClient;
+// using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using Factory.Models;
+// using System.Linq;
 
-namespace Factory.Controllers
-{
-	public class SharedController : Controller
-	{
-    private readonly FactoryContext _db;
-		public SharedController(FactoryContext db)
-		{
-			_db = db;
-		}
+// namespace Factory.Controllers
+// {
+// 	public class SharedController : Controller
+// 	{
+//     private readonly FactoryContext _db;
+// 		public SharedController(FactoryContext db)
+// 		{
+// 			_db = db;
+// 		}
 
-		[HttpGet("/")]
-		public ActionResult Index()
-		{
-      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
-			return View();
-		}
-	}
-}
+// 		[HttpGet("/")]
+// 		public ActionResult Index()
+// 		{
+//       List<Machine> machineList = _db.Machines.ToList();
+//       ViewBag.Machines = machineList;
+//       // ViewBag.Machines = new SelectList(_db.Machines, "MachineId", "Name");
+// 			return View();
+// 		}
+// 	}
+// }
