99eb4e0bc08a5a14017103c79f32182bd7fb1a52
Shannon Grantski <grantski@pm.me>
2021-01-11 19:21:04 -0800

    remove unused code in controllers

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,26 +1,7 @@
-using System.Data;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Factory.Models;
 using System.Linq;
-
-// namespace Factory.Controllers
-// {
-// 	public class HomeController : Controller
-// 	{
-
-// 		[HttpGet("/")]
-// 		public ActionResult Index()
-// 		{
-// 			return View();
-// 		}
-
-// 	}
-// }
-
 
 namespace Factory.Controllers
 {
@@ -37,7 +18,6 @@ namespace Factory.Controllers
 		{
       List<Machine> machineList = _db.Machines.ToList();
       ViewBag.Machines = machineList;
-      // ViewBag.Machines = new SelectList(_db.Machines, "MachineId", "Name");
 			return View();
 		}
 	}
